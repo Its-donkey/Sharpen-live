4e6dc43a35a93a2ecdcb3470da18b9ca86155de2
Jeff <jeff@vps-e91763aa.vps.ovh.ca>
2025-11-01 18:27:42 +1100

    fix (alerts): log pubsub challenge confirmations

--- platforms/youtube/internal/server/server.go
+++ platforms/youtube/internal/server/server.go
@@ -103,6 +103,7 @@ func (s *Server) handleVerification(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte(challenge))
+	s.logger.Printf("verification response sent: status=200 challenge=%s", challenge)
 }
 
 type httpLogger struct{}
